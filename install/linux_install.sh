3d66d1b9a592fb05de1e6180808ab83dcb933cb0
Mike MacIsaac <mike99mac@gmail.com>
2024-11-27 19:13:07 -0500

    update linux_install.sh

--- install/linux_install.sh
+++ install/linux_install.sh
@@ -70,21 +70,22 @@ function doIt
   
   echo; echo "Step 18): installing keyboard..." 
   pip install keyboard 
-  # PyDictionary seems to pull in 'futures' which causes problems...
-  # echo; echo "installing ..." 
-  # pip install PyDictionary 
-  deactivate
+
+  echo; echo "Step 19): installing pyyaml..."
+  pip install pyyaml
+
   
-  echo; echo "Step 19): installing Internet music tools ..."
+  echo; echo "Step 20): installing Internet music tools ..."
   pip install youtube-search-python
   sudo cp install/ytplay /usr/local/sbin
   sudo ln -s /usr/local/sbin/ytplay /usr/local/sbin/ytadd
   # try skipping youtube-dl - is it no longer used?
   # sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-  #sudo chmod a+rx /usr/local/bin/yotube-dl
+  # sudo chmod a+rx /usr/local/bin/yotube-dl
+  deactivate
   
   echo
-  echo "Step 20) installing Local NLP..."
+  echo "Step 21) installing Local NLP..."
   cd $HOME/minimy/framework/services/intent/nlp/local
   tar xzfv cmu_link-4.1b.tar.gz
   cd link-4.1b
@@ -92,7 +93,7 @@ function doIt
   cd ../../../../../..
   
   echo
-  echo "Step 21) installing Local STT..."
+  echo "Step 22) installing Local STT..."
   cd framework/services/stt/local/CoquiSTT/ds_model
   wget https://github.com/coqui-ai/STT-models/releases/download/english/coqui/v1.0.0-huge-vocab/huge-vocabulary.scorer
   wget https://github.com/coqui-ai/STT-models/releases/download/english/coqui/v1.0.0-huge-vocab/alphabet.txt
@@ -102,7 +103,7 @@ function doIt
   cd ../../../../..
   
   echo
-  echo "Step 22) installing Local TTS..."
+  echo "Step 23) installing Local TTS..."
   cd framework/services/tts/local
   wget http://rioespana.com/images/mimic3.tgz
   tar xzfv mimic3.tgz
@@ -114,7 +115,7 @@ function doIt
   deactivate
   
   echo
-  echo "Step 23) copying and enabling systemd .mount and .service files..." 
+  echo "Step 24) copying and enabling systemd .mount and .service files..." 
   sudo cp install/home-pi-minimy-logs.mount /etc/systemd/system 
   sudo systemctl enable home-pi-minimy-logs.mount 
   sudo cp install/home-pi-minimy-tmp.mount /etc/systemd/system 
@@ -123,7 +124,7 @@ function doIt
   sudo systemctl enable minimy
   
   echo 
-  echo "Step 24) copying Minimy scripts to /usr/local/sbin..."
+  echo "Step 25) copying Minimy scripts to /usr/local/sbin..."
   minimyScripts="startminimy stopminimy restartminimy grm cmpcode countminimy minimyver"
   cd $HOME/minimy/install
   sudo cp $minimyScripts /usr/local/sbin 
@@ -131,11 +132,11 @@ function doIt
   sudo chown $USER:$USER $minimyScripts
   
   echo 
-  echo "Step 25) copying great_songs playlist to /var/lib/mpd/playlists..."
+  echo "Step 26) copying great_songs playlist to /var/lib/mpd/playlists..."
   sudo cp $HOME/minimy/install/great_songs.m3u /var/lib/mpd/playlists
   
   echo 
-  echo "Step 26) starting virtual environment..." 
+  echo "Step 27) starting virtual environment..." 
   source $HOME/minimy/venv_ngv/bin/activate
   echo " "
   echo "Install Complete!"
