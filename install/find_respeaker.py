ad2cae868ff04c5227e389e76159e2580d00dbec
battistary <ryanskier18@icloud.com>
2026-05-27 02:13:40 -0400

    Update find_respeaker.py & installminimy

--- install/find_respeaker.py
+++ install/find_respeaker.py
@@ -2,22 +2,35 @@
 #
 # find_respeaker.py - Check if reSpeaker mic array is present
 #
-import pyaudio
 import sys
+import os
+import pyaudio
 
 def get_respeaker_index():
+  # Suppress ALSA error messages by redirecting stderr temporarily
+  stderr_fileno = sys.stderr.fileno()
+  devnull = os.open(os.devnull, os.O_WRONLY)
+  saved_stderr = os.dup(stderr_fileno)
+  os.dup2(devnull, stderr_fileno)
+
   p = pyaudio.PyAudio()
+  idx = None
   for i in range(p.get_device_count()):
     info = p.get_device_info_by_index(i)
     if info['maxInputChannels'] > 0 and "reSpeaker XVF3800" in info['name']:
-      p.terminate()
-      return i
-    p.terminate()
-    return None
+      idx = i
+      break
+  p.terminate()
+
+  # Restore stderr
+  os.dup2(saved_stderr, stderr_fileno)
+  os.close(devnull)
+  os.close(saved_stderr)
+  return idx
 
 if __name__ == "__main__":
   idx = get_respeaker_index()
   if idx is not None:
     print(idx)
-    else:
-      sys.exit(1)
+  else:
+    sys.exit(1)
